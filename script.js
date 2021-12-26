1e9e12296ad112f342277743ad625056885e7e10
Chetan More <chetan.more2907@gmail.com>
2021-12-26 09:49:40 +0530

    CallBackHellTask Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const countdown = document.querySelector("#countdown-text");
+
+let time = 10
+countdown.innerText=time--;
+setTimeout(() =>{
+    countdown.innerText=time--;
+    setTimeout(() =>{
+        countdown.innerText=time--;
+        setTimeout(() =>{
+            countdown.innerText=time--;
+            setTimeout(() =>{
+                countdown.innerText=time--;
+                setTimeout(() =>{
+                    countdown.innerText=time--;
+                    setTimeout(() =>{
+                        countdown.innerText=time--;
+                        setTimeout(() =>{
+                            countdown.innerText=time--;
+                            setTimeout(() =>{
+                                countdown.innerText=time--;
+                                setTimeout(() =>{
+                                    countdown.innerText=time--;
+                                    setTimeout(() =>{
+                                        countdown.innerText="Happy Independence Day";
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
